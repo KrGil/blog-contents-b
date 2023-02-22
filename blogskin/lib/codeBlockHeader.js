416fb3389a6151c166f2d1cb2da6616f03ebf84f
eisen <kanjjang89@naver.com>
2023-02-22 12:53:16 +0900

    Modify: profile change & minor mix

--- blogskin/lib/codeBlockHeader.js
+++ blogskin/lib/codeBlockHeader.js
@@ -1,11 +1,11 @@
 window.onload = function(){
     $(".hljs").parent().each(function () {
-        var codeheader = $("<div class='codelang'>");
-        var test = $(this).find('code').attr('class').replace('hljs', '').trim();
-        var language = '';
-        language = language == 'undefined' ? '' : test.substring(9);
-        codeheader.html('<span class="language">' + language + '</span>');
-        var codeLabel = $('<div class="codeLabel">').append('<div class="code-header"><span class="btn red"></span><span class="yellow btn"></span><span class="green btn"></span></div>').append(codeheader);
-        $(this).before(codeLabel);
+        var codeHeaderContents = $("<div class='r-code-header'>");
+        var rawLang = $(this).find('code').attr('class').replace('hljs', '').trim();
+        var language = rawLang.substring(9);
+        language = language == 'undefined' ? '' : rawLang.substring(9);
+        codeHeaderContents.html('<span class="language">' + language + '</span>');
+        var codeHeader = $('<div class="code-header">').append('<div class="l-code-header"><span class="btn red"></span><span class="yellow btn"></span><span class="green btn"></span></div>').append(codeHeaderContents);
+        $(this).before(codeHeader);
     })
 }
